71fefc03c245789ac6215bc1f32edc79557a5a93
ShoValentine <shovalentine@gmail.com>
2026-08-06 01:11:27 +0100

    Default MP tracking to nonzero transactions

--- KupoCombo/Services/IPracticePlanPolicy.cs
+++ KupoCombo/Services/IPracticePlanPolicy.cs
@@ -6,7 +6,10 @@ public interface IPracticePlanPolicy
 {
     PracticePlan BuildPracticePlan(TrainingState state);
 
-    bool TracksMpAction(uint actionId, TrainingState state);
+    bool TracksMpAction(uint actionId, TrainingState state)
+    {
+        return GetExpectedMpDelta(actionId, state) != 0;
+    }
 
     int GetExpectedMpDelta(uint actionId, TrainingState state);
 }
