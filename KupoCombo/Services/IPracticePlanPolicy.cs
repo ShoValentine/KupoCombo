95e798348415983bea732acbf33bde95c738cea4
ShoValentine <shovalentine@gmail.com>
2026-08-06 01:44:21 +0100

    Generalise practice resource policy contract

--- KupoCombo/Services/IPracticePlanPolicy.cs
+++ KupoCombo/Services/IPracticePlanPolicy.cs
@@ -1,15 +1,20 @@
+using System.Collections.Generic;
 using KupoCombo.Models;
 
 namespace KupoCombo.Services;
 
 public interface IPracticePlanPolicy
 {
+    IReadOnlyCollection<string> TrackedResources { get; }
+
     PracticePlan BuildPracticePlan(TrainingState state);
 
-    bool TracksMpAction(uint actionId, TrainingState state)
-    {
-        return GetExpectedMpDelta(actionId, state) != 0;
-    }
+    IReadOnlyDictionary<string, int> GetExpectedResourceDeltas(
+        uint actionId,
+        TrainingState state);
 
-    int GetExpectedMpDelta(uint actionId, TrainingState state);
+    bool TracksResourceAction(uint actionId, TrainingState state)
+    {
+        return GetExpectedResourceDeltas(actionId, state).Count > 0;
+    }
 }
